142f7a97f2f1cc0697a367fdec142e4aeaaa6b89
Andres Lopez <andres.lopez@e-hps.com>
2018-11-05 14:27:58 -0500

    esperas implicitas

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -17,6 +17,7 @@ export const config: Config = {
   },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
     reporter();
   },
 };
--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -11,6 +11,7 @@ export const config: Config = {
   },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
     reporter();
   }
 };
