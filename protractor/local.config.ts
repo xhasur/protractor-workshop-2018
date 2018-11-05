df7e57d5ac5b3841fd11ac52c7b19e8f8ba74fb5
Andres Lopez R <andreslopez.exe@gmail.com>
2018-11-05 14:21:12 -0500

    esperas carga de pagina (#12)

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -4,8 +4,11 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 1000,
   SELENIUM_PROMISE_MANAGER: false,
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
