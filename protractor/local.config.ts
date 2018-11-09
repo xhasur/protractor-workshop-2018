305c629e794d1ff8a3293ed34776c11a752775d7
Andres Lopez Restrepo <andres.lopez@e-hps.com>
2018-11-09 13:04:39 -0500

    cpabilities

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -9,6 +9,13 @@ export const config: Config = {
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
   },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     browser.manage().timeouts().implicitlyWait(0);
