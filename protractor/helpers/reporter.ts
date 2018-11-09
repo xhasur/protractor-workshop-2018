0b84665959a46246ed589a230b8ed4124be91c74
Andres Lopez R <andreslopez.exe@gmail.com>
2018-11-09 15:42:15 -0500

    17 utilizando capabilities chrome (#18)
    * awesome config
    
    * cpabilities

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,4 +1,5 @@
 import { SpecReporter } from 'jasmine-spec-reporter';
+const { AwesomeReport } = require('jasmine-awesome-report');
 
 export let reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
@@ -6,4 +7,12 @@ export let reporter = () => {
       displayStacktrace: true
     }
   }));
+
+  const config = {
+    fullPath: 'reports',
+    fileName: 'awesome',
+    merge: true
+  };
+
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 };
